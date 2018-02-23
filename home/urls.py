ade933daac809bc47ff3e31d52769552d8608611
Nyghtwel <nyghtwel@gmail.com>
2018-02-23 04:27:31 -0500

    committed out local_setting database bc of firewall, add basic UI elements

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('query1', views.query1, name='query1'),
+    path('data_set', views.data_set, name='data_set'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 def index(request):
     # return HttpResponse("hello from index")
     return render(request, 'home/index.html')
+
+
+def query1(request):
+    return render(request, 'home/query1.html')
+
+def about(request):
+    return render(request, 'home/about.html')
+
+def data_set(request):
+    return render(request, 'home/data_set.html')
