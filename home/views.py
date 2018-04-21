426417108a7952f0ee393b88fa0282b6312fda1f
Nyghtwel <nyghtwel@gmail.com>
2018-04-20 23:07:43 -0400

    added bar graph for query1

--- home/views.py
+++ home/views.py
@@ -48,8 +48,10 @@ def query1(request):
 	global ans2 
 	global ans3
 	global ans4 
+	global ans5
 	btn_class = 'btn btn-success disabled'
 	ans, query_title, query = [], "", ""
+	national_avg = ""
 	# if ans1 == "":
 		# ans1 = "temp"
 	for i in query1_content:
@@ -101,7 +103,7 @@ def query1(request):
 		messages.success(request, query)
 
 	if request.method == 'POST' and request.POST.get("Above/Below"):
-		query1_content[4]['save'] = request.POST.get("Above/Below")
+		query1_content[4]['save'] = ans5 = request.POST.get("Above/Below")
 		btn_class = 'btn btn-success'
 
 	if request.method == 'POST' and request.POST.get("submit"):
@@ -115,7 +117,7 @@ def query1(request):
 							from chronic_disease_indicator 
 							where name = '{}')) select location.name, temp_nat.year_start as year, concat(round((select avg(data_value) 
 								from temp_nat), 1), '%') as national_avg, 
-								concat(temp_nat.data_value, temp_nat.data_unit) as location_data_value from temp_nat, location 
+								temp_nat.data_value as location_data_value from temp_nat, location 
 									where temp_nat.location_id = location.location_ID and temp_nat.data_value {} (select avg(data_value) from temp_nat)
 				""".format(ans3, ans4, ans2, temp)
 		
@@ -124,15 +126,20 @@ def query1(request):
 			ans = dictfetchall(cursor)
 
 		messages.success(request, query_title)
-		
+		national_avg = ans[0]['NATIONAL_AVG']
+
 		for i in query1_content:
 			i['fields'], i['disabled'], i['save'] = [], "disabled", ""
 
-		ans1 = ans2 = ans3 = ans4 = ans5 = ""
+		# ans1 = ans2 = ans3 = ans4 = ans5 = ""
 		query1_content[0]['fields'], query1_content[0]['disabled'] = populate_form('NAME', "Select distinct name from health_domain")
 
 	for i in query1_content:
 		i['fields'].insert(0, i['save'])
+
+	import json
+	json_data = json.dumps(ans)
+	# print(json_data)
 
 	# print(request.session.get('topic'))	
 	context = {
@@ -142,8 +149,10 @@ def query1(request):
 		'ans2': (ans2 if ans2 else ""),
 		'ans3': (ans3 if ans3 else ""),
 		'ans4': (ans4 if ans4 else ""), 
-		
-		'btn_class' : btn_class
+		'ans5': (ans5 if ans5 else ""),
+		'json_data': json_data,	
+		'btn_class' : btn_class,
+		'national_avg': national_avg
 		
 	}
 	return render(request, 'home/query1.html', context)
@@ -355,6 +364,9 @@ def query3(request):
 		with connection.cursor() as cursor:
 			cursor.execute(query_title)
 			ans = dictfetchall(cursor)
+		
+		print(ans)			
+		print(query_title)
 		messages.success(request, query_title)
 		for i in query3_content:
 			i['fields'], i['disabled'], i['save'] = [], "btn btn-success disabled", ''
@@ -433,7 +445,7 @@ def query4(request):
 
 	if request.method == 'POST' and request.POST.get("Year End"):
 		query4_content[4]['save'] = ans5 = request.POST.get("Year End")
-		query4_content[5]['fields'], query4_content[5]['disabled'] = ["increase", "decrease"], "btn btn-success"
+		query4_content[5]['fields'], query4_content[5]['disabled'] = ["increase", "decrease"], ""
 		for i in query4_content[6:]:
 			i['disabled'] = 'disabled'
 		messages.success(request, query)
