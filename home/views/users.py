b9ba3d771d111a945d0a07ddb02dae7aec2e7486
Nyghtwel <nyghtwel@gmail.com>
2018-04-24 02:04:47 -0400

    login, logout, sign in

--- home/views/users.py
+++ home/views/users.py
@@ -20,42 +20,94 @@ def users(request):
             if request.POST.get(option):
                 print(option)
                 next = request.POST.get(option, '/')
+                msg = ""
                 if option == 'sign up': 
-                    add_user(request)
+                    username = request.POST.get('user')
+                    password = request.POST.get('pwd')
+                    if add_user(username, password): 
+                        msg = 'User added'
+                        settings.USER = True
+                        settings.USER_NAME = username
+                    else: msg = 'Failed to add user'
+                    print(msg)
+
                 if option == 'login':
-                    settings.USER = True
+                    print('login')
+                    print(request.POST)
+                    username = request.POST.get('user')
+                    password = request.POST.get('pwd')
+                    if (validate_login(username, password)):
+                        msg = 'Login successful'                      
+                        settings.USER = True
+                        settings.USER_NAME = username
+                    else:
+                        msg = 'Failed to login'
+
                 if option == 'logout': 
+                    settings.USER = False
                     logout_user(request)
+                    msg = 'Logged out'
+                
+                messages.success(request, msg)
                 return HttpResponseRedirect(next)
         
-        
+    user_history = show_user_history(settings.USER_NAME)
+    print(user_history)
     context = {
-
+        'user_history': user_history
     }
     return render(request, 'home/users.html', context)
 
-def add_user(request):
-    print('user')
+def add_user(username, password):
+    
+    if not validate_username(username): return False
+    query = '''insert into users(username, password) values('{}', '{}')
+    '''.format(username, password)
+
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+    return True
     # user = User.objects.create_user(username='test2', password='password') 
     # print(user)
 
-def get_user(request):
-    user = authenticate(request, username='test', password='dummy')
-    # login(user)
-    print('login')
 
-def show_user():
-    pass
+def validate_username(input):
+    query = ''' select username from users where username = '{}'
+        '''. format(input)
+    ans = None
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        ans = cursor.fetchone()
+    
+    print(ans)
+    if ans == input: return False
+    else: return True
 
-def update_user(parameter_list):
-    pass
+def validate_login(username, password):
+    query = ''' select username from users where username = '{}' and password = '{}' 
+            '''. format(username, password)
+    ans = None
+    print(query)
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        ans = cursor.fetchone()
+    print('validate login')
+    print(ans)   
+    if ans: return True
+    else: return False     
+     
+def show_user_history(show_list):
+    # if request.method == 'POST' and request.POST.get('submit'):
+    username = show_list[0]
+    query = '''select write.username, write.datetime, query.query from query, write 
+            where query.query_id = write.query_id and 
+            write.username ='{}' '''.format(username)
+    
+    ans = None
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        ans = dictfetchall(cursor)
+    
+    return ans
 
-def delete_user(parameter_list):
-    pass
 
-def show_user_history(parameter_list):
-    pass
-
-def logout_user(request):
-    print('logout')
-    # logout(request)
